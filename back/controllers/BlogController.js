4ae56cce0667373982a3745cf072f7d09d33467f
LucasL72 <lucasj.ledoux@gmail.com>
2021-12-13 16:56:17 +0100

    fix

--- back/controllers/BlogController.js
+++ back/controllers/BlogController.js
@@ -1,28 +1,26 @@
 /*
  * Controller: Blog & ID
- * **************** */ 
+ * **************** */
 
 exports.blogpage = (req, res) => {
-    console.log('je suis la page blog')
+  console.log('je suis la page blog')
   res.render("blog");
 };
 
-
-
-/** Controller: ID* **************** */ 
+/** Controller: ID* **************** */
 
 exports.idpage = (req, res) => {
   console.log('je suis la page Id')
-res.render("blog-id");
+  res.render("blog-id");
 };
 
 exports.CreateCom = (req, res) => {
-  console.log("Je suis le controller Create Com dans blog-id",req.body);
+  console.log("Je suis le controller Create Com dans blog-id", req.body);
   res.render("blog-id");
 }
 
 exports.EditCom = (req, res) => {
-  console.log("Je suis le controller Edit Com dans blog-id",req.params.id,req.body);
+  console.log("Je suis le controller Edit Com dans blog-id", req.params.id, req.body);
   res.render("blog-id");
+  // res.redirect('/blog-id')
 }
-
