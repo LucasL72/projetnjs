84685bab3f95a8dc03330d912d950282bcb06b48
LucasL72 <lucasj.ledoux@gmail.com>
2022-02-21 11:59:41 +0100

    test mocha

--- test/test.js
+++ test/test.js
@@ -3,29 +3,64 @@ const {
     query
 } = require("../serverl");
 
-describe("MOCHA // CRUD // user", () => {
-    let user = {};
+describe("MOCHA // CRUD // articles", () => {
+    let article = {};
     let id = 0;
 
     // Loop for create Customer before 'it'
     beforeEach(async () => {
-        let values = ["BRUNO", "Bru@nu.fr"];
-        let sql = `INSERT INTO user (name,email) values(?)`;
-        const user = await query(sql, [values]);
+        let values = ["mon image","BRUNO", "test bruno","test bruno","0"];
+        let sql = `INSERT INTO articles (imgarticle,title,description,contenu,user_id) values("mon image","BRUNO","test bruno","test bruno",0);`;
+        const Articles = await db.query(sql, [values]);
 
         // console.log("Before EACH: ", user);
-        assert.ok(user.insertId);
+        assert.ok(Articles.insertId);
 
-        const userID = await query(`SELECT * FROM user where id = ${ user.insertId }`)
-        user = userID[0]
-        assert.strictEqual(userID[0].name, "BRUNO");
-        assert.strictEqual(userID[0].email, "Bru@nu.fr");
+        const ArticlesID = await db.query(`SELECT * FROM articles where id = ${ Articles.insertId }`)
+        article = ArticlesID[0]
+        assert.strictEqual(ArticlesID[0].imgarticle, "mon image");
+        assert.strictEqual(ArticlesID[0].title, "BRUNO");
+        assert.strictEqual(ArticlesID[0].description, "test bruno");
+        assert.strictEqual(ArticlesID[0].contenu, "test bruno");
+        assert.strictEqual(ArticlesID[0].user_id, 0);
 
     });
     // Test
-  it("TEST // user", (done) => {
-    // console.log("TEST: ", id)
+  it("TEST // Articles", (done) => {
+     console.log("TEST: ", id)
     done();
   });
-// test
+ // Create Customer
+ it("POST // articles", async () => {
+    let values = ["mon image","john", "test john","test john","0"];
+    let sql = `INSERT INTO articles (imgarticle,title,description,contenu,user_id) values("mon image","john","test john","test john",1);`;
+    const Articles = await db.query(sql, [values]);
+
+     console.log("POST: ", Articles.insertId)
+
+    assert.ok(Articles);
+
+    const ArticlesID = await db.query(
+      `SELECT * FROM articles where id = ${Articles.insertId}`
+    );
+    assert.strictEqual(ArticlesID[0].imgarticle, "mon image");
+        assert.strictEqual(ArticlesID[0].title, "john");
+        assert.strictEqual(ArticlesID[0].description, "test john");
+        assert.strictEqual(ArticlesID[0].contenu, "test john");
+        assert.strictEqual(ArticlesID[0].user_id, 1);
+  });
+
+  // Get ALL Customer
+  it("GET ALL // articles", async () => {
+    let sql = `SELECT * FROM articles`;
+    const listArticles = await db.query(sql);
+
+     console.log('GET ALL: ', listArticles)
+
+    assert.ok(listArticles);
+
+    const Articles = await db.query(`SELECT * FROM articles`);
+    assert.strictEqual(Articles.length > 0, true);
+  });
+
 });
