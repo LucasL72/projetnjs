85cbe6eac483a43f506a2bf2be057a18bb921ab9
LucasL72 <lucasj.ledoux@gmail.com>
2022-02-21 11:12:22 +0100

    debut test

--- test/test.js
+++ test/test.js
@@ -4,23 +4,28 @@ const {
 } = require("../serverl");
 
 describe("MOCHA // CRUD // user", () => {
-    let customer = {};
+    let user = {};
     let id = 0;
 
     // Loop for create Customer before 'it'
     beforeEach(async () => {
-        let values = ["BRUNO", "Bru@nu.fr", "0606060606"];
-        let sql = `INSERT INTO customers (name,email,mobile) values(?)`;
+        let values = ["BRUNO", "Bru@nu.fr"];
+        let sql = `INSERT INTO user (name,email) values(?)`;
         const user = await query(sql, [values]);
 
         // console.log("Before EACH: ", user);
         assert.ok(user.insertId);
 
-        const userID = await query(`SELECT * FROM customers where id = ${ user.insertId }`)
-        customer = userID[0]
+        const userID = await query(`SELECT * FROM user where id = ${ user.insertId }`)
+        user = userID[0]
         assert.strictEqual(userID[0].name, "BRUNO");
         assert.strictEqual(userID[0].email, "Bru@nu.fr");
 
     });
+    // Test
+  it("TEST // user", (done) => {
+    // console.log("TEST: ", id)
+    done();
+  });
 
 });
