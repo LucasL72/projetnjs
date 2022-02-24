b9f5c8e4cf3339aec6e3ccb1df312af061d0e7cf
LucasL72 <lucasj.ledoux@gmail.com>
2022-02-24 16:55:55 +0100

    debut sharp

--- back/config/sharpPics.js
+++ back/config/sharpPics.js
@@ -4,17 +4,17 @@ const sharp = require('sharp'),
 
 module.exports = (req, res, next) => {
     if (req.file || req.files) {
-        for (let i = 0; i < req.files.length; i++) {
+        for (let i = 0; i < req.file.length; i++) {
             let pathSharp = "./public/data/photos/"
-            const file = req.file;
+            const file = req.files;
 
             sharp(file[i].path)
                 .resize({ fit: sharp.fit.cover})
                 .webp({ quality: 100 })
-                .toFile(pathSharp + files[i].filename.split('.').slice(0, -1).join('.') + ".webp", (err, info) => { })
+                .toFile(pathSharp + file[i].filename.split('.').slice(0, -1).join('.') + ".webp", (err, info) => { })
                 .toBuffer()
                 .then(() => {
-                    pathImg = path.resolve("public/images/photos/" + req.files[i].filename)
+                    pathImg = path.resolve("./public/data/photos/" + req.files[i].filename)
                     fs.unlink(pathImg, (err) => {
                         if (err) console.log(err)
                     })
