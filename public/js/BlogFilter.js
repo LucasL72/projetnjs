8112798a62bb877b79b9080b43e2c2ff76592915
LucasL72 <lucasj.ledoux@gmail.com>
2022-02-28 14:59:16 +0100

    edit blogfilter

--- public/js/BlogFilter.js
+++ public/js/BlogFilter.js
@@ -1,7 +1,7 @@
 /*======== La fonction filtre par titre ======================*/
 function monFiltre() {
     // Declare variables
-    let input, filter, listCard, card, h4, i, txtValue;
+    let input, filter, listCard, card, h3, i, txtValue;
     input = document.getElementById('inputFilter');
     filter = input.value.toUpperCase();
     listCard = document.getElementById("listCard");
@@ -9,8 +9,8 @@ function monFiltre() {
 
     // Loop through all list items, and hide those who don't match the search query
     for (i = 0; i < card.length; i++) {
-        h4 = card[i].getElementsByTagName("h4")[0];
-        txtValue = h4.textContent || h4.innerText;
+        h3 = card[i].getElementsByTagName("h3")[0];
+        txtValue = h3.textContent || h3.innerText;
 
         if (txtValue.toUpperCase().indexOf(filter) > -1) {
             card[i].style.display = "";
